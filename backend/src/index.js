8248ec54248bd50ced677db1640884bcd709fb1f
Kenny <kennywheat96@gmail.com>
2026-03-30 19:01:10 -0500

    Update CORS for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,10 @@ initializeSocket(httpServer);
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://spotify-clone-gts5.vercel.app"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Range"],

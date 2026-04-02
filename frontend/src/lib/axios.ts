af9a1fe2f87998a25b975092ccc9d721e1d2c100
Kenny <kennywheat96@gmail.com>
2026-04-02 00:33:53 -0500

    fix: hardcode Render URL as baseURL

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,11 +2,7 @@ import axios from "axios";
 import { useAuth } from "@clerk/clerk-react";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL 
-    ? `${import.meta.env.VITE_API_URL}/api`
-    : import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api"
-    : "https://spotify-clone-q2iy.onrender.com/api",
+  baseURL: "https://spotify-clone-q2iy.onrender.com/api",
 });
 
 export const useAxiosWithAuth = () => {
